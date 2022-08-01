963886da538ee32f4a0c9b8e43b3269afafef17e
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-02 01:18:35 +0900

    新規登録画面作成

---
+++ app/models/department.rb
@@ -0,0 +1,12 @@
+class Department < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '本社1課' },
+    { id: 3, name: '本社1課' },
+    { id: 4, name: '西東京支店' },
+    { id: 5, name: '千葉支店' },
+    { id: 6, name: '埼玉支店' },
+  ]
+  include ActiveHash::Associations
+  has_many :users
+  end
