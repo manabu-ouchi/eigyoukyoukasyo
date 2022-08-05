6f9d5841d3fd57fdff626a8ecdf567e91f40e486
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-05 18:33:40 +0900

    投稿のバリデーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   belongs_to :user
