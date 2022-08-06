f8505b26a98acf7f0b643e74b4b3a59631e1e5fb
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-07 04:24:03 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   get '/posts/genre/:id', to: "posts#genre"
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :show]
 end
