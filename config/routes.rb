89425aadfabf0241124691253a0547943442c1e8
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-23 18:30:39 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   get '/posts/genre/:id', to: "posts#genre"
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :records, only: [:index, :new, :create]
   resources :users, only: :show
 end
