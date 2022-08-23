7295d70a4100146e115566b42c34d57ae37bd3aa
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-23 22:13:56 +0900

    コントローラー記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def create
      
     @comment = Comment.create(comment_params)
@@ -11,10 +12,17 @@ class CommentsController < ApplicationController
       render "psots/show"
 
     end
-    
-
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+   if @comment.destroy
+     redirect_to post_path(@post)
+   end
+  end  
+
+private
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id,  post_id: params[:post_id])
   end
