a6b41a99b1c453bfe2059048275b4c2b6e0e1d77
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-28 02:23:33 +0900

    投稿(質問)を削除するとに紐づくコメント(回答)も一緒に削除されるように記述

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,5 +6,5 @@ class Question < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   belongs_to :user 
-  has_many :answers
+  has_many :answers, dependent: :destroy
 end
