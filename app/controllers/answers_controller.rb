88c205b2edc7187a37f5eeb060e835fda54bd53c
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-28 02:11:52 +0900

    コントローラー記述

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -15,6 +15,14 @@ class AnswersController < ApplicationController
     end
   end
 
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+   if @answer.destroy
+     redirect_to question_path(@question)
+   end
+  end  
+
   private
   def answer_params
     params.require(:answer).permit(:answer).merge(user_id: current_user.id,  question_id: params[:question_id])
