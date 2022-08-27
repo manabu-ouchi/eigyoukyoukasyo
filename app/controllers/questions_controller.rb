31a44e73b1c8fd8e7c7c5e8675bb3b1305b5173b
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-08-27 19:35:20 +0900

    アクション実行の制限記述

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   def index
