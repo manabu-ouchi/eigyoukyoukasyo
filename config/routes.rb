d14f1a62c4a23e2ade69329a01b7c5c1b56c02b3
manabu-ouchi <mochi.mochi.pillow@gmail.com>
2022-09-05 16:59:10 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   get '/posts/genre/:id', to: "posts#genre"
+  resources :users, only: :show
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
